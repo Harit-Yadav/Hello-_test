26365bc6b841dbcbc8fa12f75c2c5302fde71b24
Harit-Yadav <harityadav679@gmail.com>
2024-08-03 17:04:19 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+  
+  int git = 65;
+  
+  int github = 70;
+  
+    if( git < github ){
+        
+        cout<<"hello git";
+    }
+else { 
+    cout<<"no problem";
+   
+    return 0;
+}
+cout<<"thankyou";
+}
